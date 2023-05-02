57a4fe3523067f2c4e8fdedfae0b785b6b36cd7e
Sayantan <sayantan.das208@gmail.com>
2023-05-02 23:47:33 +0530

    Update SelectionSort.java

--- src/sorting/SelectionSort.java
+++ src/sorting/SelectionSort.java
@@ -1,3 +1,17 @@
+/*
+Given an unsorted array of size N, use selection sort to sort arr[] in increasing order.
+
+1. We will select the range of the unsorted array using a loop (say i) that indicates the starting index of the range.
+2. The loop will run forward from 0 to n-1. The value i = 0 means the range is from 0 to n-1, and similarly, i = 1 means the range is from 1 to n-1, and so on.
+(Initially, the range will be the whole array starting from the first index.)
+3. In each iteration, we will select the minimum element from the range of the unsorted array using an inner loop.
+4. We will swap the minimum element with the first element of the selected range(in step 1).
+5. After each iteration, we will find that the array is sorted up to the first index of the range.
+
+TC: O(n2)
+SC: O(1)
+ */
+
 package sorting;
 
 public class SelectionSort {
