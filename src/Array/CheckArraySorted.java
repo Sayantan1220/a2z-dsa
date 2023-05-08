fce8d916a4bb95410be2a71b86fa8e64dc583bad
Sayantan <sayantan.das208@gmail.com>
2023-05-09 00:14:07 +0530

    If array is sorted

---
+++ src/Array/CheckArraySorted.java
@@ -0,0 +1,21 @@
+package Array;
+
+public class CheckArraySorted {
+
+    static boolean arraySortedOrNot(int[] arr, int n) {
+        // code here
+        for (int i = 0; i < n - 1; i++){
+            if (arr[i] > arr[i+1]){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 8, 87, 56, 90};
+        int size = arr.length;
+        boolean result = arraySortedOrNot(arr, size);
+        System.out.println(result);
+    }
+}
