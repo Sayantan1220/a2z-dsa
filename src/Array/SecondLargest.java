667a51117d17419521e07025675b267ecc856f14
Sayantan <sayantan.das208@gmail.com>
2023-05-09 00:01:57 +0530

    2nd Largest element in array

---
+++ src/Array/SecondLargest.java
@@ -0,0 +1,28 @@
+package Array;
+
+public class SecondLargest {
+
+    static int print2largest(int[] arr, int n) {
+        // code here
+        int max = 0;
+        int secondMax = 0;
+        for (int i = 0; i < n; i++){
+            if (arr[i] > max){
+                max = arr[i];
+            }
+        }
+        for (int i = 0; i < n; i++){
+            if (arr[i] < max && arr[i] > secondMax){
+                secondMax = arr[i];
+            }
+        }
+        return secondMax;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {12, 35, 1, 10, 34, 1};
+        int size = arr.length;
+        int result = print2largest(arr, size);
+        System.out.println(result);
+    }
+}
