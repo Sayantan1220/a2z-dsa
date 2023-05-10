c0b6a81391f846ce629191625a547acb2887c6da
Sayantan <sayantan.das208@gmail.com>
2023-05-10 11:57:39 +0530

    Rotate Array

--- src/Array/RotateArray.java
+++ src/Array/RotateArray.java
@@ -1,3 +1,11 @@
+/*
+Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
+https://leetcode.com/problems/rotate-array/description/
+
+TC: O(n)
+SC: O(1)
+ */
+
 package Array;
 
 public class RotateArray {
