74e4fe328bb8c5a3b7718bec860b23598a7b17e3
Sayantan <sayantan.das208@gmail.com>
2023-05-10 15:38:39 +0530

    Move Zeros

---
+++ src/Array/MoveZeroes.java
@@ -0,0 +1,45 @@
+package Array;
+
+public class MoveZeroes {
+
+    public static void zerosToEnd(int[] arr, int n) {
+
+        //finding first zero occurrence
+        int k = 0;
+        while (k < n) {
+            if (arr[k] == 0) {
+                break;
+            } else {
+                k = k + 1;
+            }
+        }
+
+        //finding zeros and immediate non-zero elements and swapping them
+        int i = k;
+        int j = k + 1;
+        while (i < n && j < n) {
+            if (arr[j] != 0) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                i++;
+            }
+            j++;
+        }
+    }
+
+    static void printArray(int[] arr, int size){
+        for (int i = 0; i < size; i++){
+            System.out.println(arr[i]);
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {0,1,0,3,12};
+        int size = arr.length;
+        printArray(arr, size);
+        zerosToEnd(arr, size);
+        printArray(arr, size);
+    }
+}
--- src/Array/RotateArray.java
+++ src/Array/RotateArray.java
@@ -38,7 +38,7 @@ public class RotateArray {
     public static void main(String[] args) {
         int[] arr = {1,2,3,4,5,6,7};
         int size = arr.length;
-        int k = 3;
+        int k = 2;
         printArray(arr, size);
         rotate(arr, size, k);
         printArray(arr, size);
