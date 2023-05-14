9c66b615920f2b8ec798aeaa76d2d7ae76d6c1e4
Sayantan <sayantan.das208@gmail.com>
2023-05-14 15:36:53 +0530

    Majority Element

--- src/Array/MajorityElement.java
+++ src/Array/MajorityElement.java
@@ -1,15 +1,25 @@
+/*
+Given an array nums of size n, return the majority element.
+The majority element is the element that appears more than ⌊n / 2⌋ times.
+You may assume that the majority element always exists in the array.
+https://leetcode.com/problems/majority-element/description/
+
+TC: O(n)
+SC: O(1)
+ */
+
 package Array;
 
 public class MajorityElement {
     public static int majorityElement(int[] nums) {
         int count=0;
-        Integer me=null;
+        int me=0;
 
         //travese the array
         //make the first ele as the majority
         //if next is qual c++ || if not c--
         // after count bcom zero make the element as majority where count got zero
-        for(int i=0;i<nums.length;i++){
+        for (int i=0;i<nums.length;i++){
             if(count==0){
                 me=nums[i];
                 count=1;
