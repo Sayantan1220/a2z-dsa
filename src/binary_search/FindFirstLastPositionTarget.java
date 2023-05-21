def77b56a163e90fc1163f01ac3eb17eb2982bee
Sayantan <sayantan.das208@gmail.com>
2023-05-21 20:41:37 +0530

    Search Range

--- src/binary_search/FindFirstLastPositionTarget.java
+++ src/binary_search/FindFirstLastPositionTarget.java
@@ -1,3 +1,35 @@
+/*
+Given an array of integers nums sorted in non-decreasing order,
+find the starting and ending position of a given target value.
+If target is not found in the array, return [-1, -1].
+You must write an algorithm with O(log n) runtime complexity.
+https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/description/
+
+Approach:
+1. Initialize the answer array result with [-1, -1] to represent the starting and ending indices of the target value.
+2. Initialize start as 0, end as the last index of the nums array.
+3. Calculate mid as the floor division of low + (high - low)/2 (Because of int overflow)
+4. Run a binary search loop until start becomes greater than end.
+5. Check if the target value is equal to the element at the mid index of nums.
+    If true, update ans[0] (starting index) with mid and move the end pointer to mid - 1 to continue searching for the leftmost occurrence of the target.
+    If false, check if the target value is less than the element at the mid index of nums.
+    If true, update end to mid - 1 to narrow down the search range to the left half.
+    If false, update start to mid + 1 to narrow down the search range to the right half.
+6. Reset start to 0 and end to the last index of the nums array.
+7. Calculate mid as the floor division of low + (high - low)/2 (Because of int overflow)
+8. Run another binary search loop until start becomes greater than end.
+9. Check if the target value is equal to the element at the mid index of nums.
+    If true, update ans[1] (ending index) with mid and move the start pointer to mid + 1 to continue searching for the rightmost occurrence of the target.
+    If false, check if the target value is less than the element at the mid index of nums.
+    If true, update end to mid - 1 to narrow down the search range to the left half.
+    If false, update start to mid + 1 to narrow down the search range to the right half.
+10. Return the ans vector containing the starting and ending indices of the target value.
+
+TC: O(log n)
+SC: O(1)
+ */
+
+
 package binary_search;
 
 import java.util.Arrays;
