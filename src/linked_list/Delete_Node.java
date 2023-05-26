c3846fdc3c8f21c9ff50bd1c61993e621d4a2dc8
Sayantan <sayantan.das208@gmail.com>
2023-05-26 21:09:41 +0530

    Delete Node

--- src/linked_list/Delete_Node.java
+++ src/linked_list/Delete_Node.java
@@ -1,4 +1,16 @@
 package linked_list;
 
 public class Delete_Node {
+    int val;
+    Delete_Node next;
+    public void deleteNode(Delete_Node node) {
+        while(node.next.next != null){
+            node.val = node.next.val;
+            node = node.next;
+        }
+        node.val = node.next.val;
+        node.next = null;
+    }
+
+
 }
