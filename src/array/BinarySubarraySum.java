eb95721fa74a352d568180daf583af7b93ad34e5
Sayantan <sayantan.das208@gmail.com>
2023-05-30 23:31:41 +0530

    Subarray sum

--- src/array/BinarySubarraySum.java
+++ src/array/BinarySubarraySum.java
@@ -1,3 +1,12 @@
+/*
+Given a binary array nums and an integer goal, return the number of non-empty subarrays with a sum goal.
+https://leetcode.com/problems/binary-subarrays-with-sum/description/
+
+TC: O(n)
+SC: O(n)
+ */
+
+
 package array;
 
 import java.util.HashMap;
