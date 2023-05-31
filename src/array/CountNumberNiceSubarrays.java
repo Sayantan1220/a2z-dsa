e057cf0998b70d3d4ef052dfb6b8599e35152108
Sayantan <sayantan.das208@gmail.com>
2023-05-31 23:27:52 +0530

    Count Number of Nice Subarrays

--- src/array/CountNumberNiceSubarrays.java
+++ src/array/CountNumberNiceSubarrays.java
@@ -1,3 +1,13 @@
+/*
+Given an array of integers nums and an integer k.
+A continuous subarray is called nice if there are k odd numbers on it.
+Return the number of nice sub-arrays.
+https://leetcode.com/problems/count-number-of-nice-subarrays/description/
+
+TC: O(n)
+SC: O(1)
+ */
+
 package array;
 
 public class CountNumberNiceSubarrays {
