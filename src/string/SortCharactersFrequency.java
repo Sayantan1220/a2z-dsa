09dc6ae5f466021762c9005ca31da35268d3d469
Sayantan <sayantan.das208@gmail.com>
2023-06-06 22:27:00 +0530

    Sort Characters by Frequency

--- src/string/SortCharactersFrequency.java
+++ src/string/SortCharactersFrequency.java
@@ -1,3 +1,13 @@
+/*
+Given a string s, sort it in decreasing order based on the frequency of the characters.
+The frequency of a character is the number of times it appears in the string.
+Return the sorted string. If there are multiple answers, return any of them.
+https://leetcode.com/problems/sort-characters-by-frequency/description/
+
+TC: O(n)
+SC: O(n)
+ */
+
 package string;
 
 import java.util.HashMap;
