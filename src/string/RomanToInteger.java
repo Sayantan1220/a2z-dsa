66251d1cf0cf1e87f92dee849baa2d03ba1196ff
Sayantan <sayantan.das208@gmail.com>
2023-06-07 23:10:54 +0530

    Roman to integer

--- src/string/RomanToInteger.java
+++ src/string/RomanToInteger.java
@@ -1,4 +1,29 @@
 package string;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class RomanToInteger {
+
+    public int romanToInt(String s) {
+        Map<Character, Integer> map = new HashMap<>() {{
+            put('I', 1);
+            put('V', 5);
+            put('X', 10);
+            put('L', 50);
+            put('C', 100);
+            put('D', 500);
+            put('M', 1000);
+        }};
+        int result = 0, n = s.length();
+
+        for (int i = 0; i < n; i++) {
+            if (i < n - 1 && map.get(s.charAt(i)) < map.get(s.charAt(i + 1))) {
+                result -= map.get(s.charAt(i));
+            } else {
+                result += map.get(s.charAt(i));
+            }
+        }
+        return result;
+    }
 }
