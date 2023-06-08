6d414147bec838da5e9219bba505bb1f391c4586
Sayantan <sayantan.das208@gmail.com>
2023-06-08 23:41:35 +0530

    Sum of Beauty all substrings

--- src/string/SumBeautyAllSubstrings.java
+++ src/string/SumBeautyAllSubstrings.java
@@ -6,8 +6,7 @@ public class SumBeautyAllSubstrings {
         //using an array as frequency :-
         int sum = 0 ;
         for(int i = 0 ; i < s.length() ; i++){
-            int[] frequency = new int[26] ;//for 26 alphabets ;
-
+            int[] frequency = new int[26] ;//for 26 alphabets
             for(int j = i ; j<s.length() ;j++){
                 frequency[s.charAt(j)-'a']++ ;
                 //now beauty from substring i to j :-
@@ -19,9 +18,9 @@ public class SumBeautyAllSubstrings {
     }
 
     public int beauty(int[] frequency){
-        int min = Integer.MAX_VALUE; int max = Integer.MIN_VALUE ;
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE ;
         for (int j : frequency) {
-
             if (j != 0) {//this is very very very important
                 min = Math.min(j, min);
             }
