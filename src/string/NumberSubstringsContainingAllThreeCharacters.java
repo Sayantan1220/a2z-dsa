7a172be096c73fcc036f950f21ef2e612bc9b87f
Sayantan <sayantan.das208@gmail.com>
2023-06-09 19:30:59 +0530

    Number of substrings containing all three characters

--- src/string/NumberSubstringsContainingAllThreeCharacters.java
+++ src/string/NumberSubstringsContainingAllThreeCharacters.java
@@ -1,4 +1,32 @@
 package string;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class NumberSubstringsContainingAllThreeCharacters {
+
+    public int numberOfSubstrings(String s) {
+        int n=s.length();
+        int i=0;
+        int j=0;
+        int ans=0;
+        Map<Character,Integer> map=new HashMap<>();
+        while(j<n){
+            map.put(s.charAt(j),map.getOrDefault(s.charAt(j),0)+1);
+            // System.out.println(map);
+            if (map.containsKey('a') && map.containsKey('b') && map.containsKey('c')) {
+                while (map.containsKey('a') && map.containsKey('b') && map.containsKey('c')) {
+                    ans += n - j;
+                    char ch = s.charAt(i);
+                    map.put(ch, map.getOrDefault(ch, 0) - 1);
+                    if (map.get(ch) == 0) {
+                        map.remove(ch);
+                    }
+                    i++;
+                }
+            }
+            j++;
+        }
+        return ans;
+    }
 }
