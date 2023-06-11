70e9692093d6cf8e4bcdae3b1bd732415e99de39
Sayantan <sayantan.das208@gmail.com>
2023-06-11 21:02:31 +0530

    Letter combinations of a phone number

--- src/Recursion/LetterCombinationsPhoneNumber.java
+++ src/Recursion/LetterCombinationsPhoneNumber.java
@@ -1,3 +1,15 @@
+/*
+Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent.
+Return the answer in any order.
+A mapping of digits to letters (just like on the telephone buttons) is given below.
+Note that 1 does not map to any letters.
+https://leetcode.com/problems/letter-combinations-of-a-phone-number/description/
+
+TC: O(n)
+SC: O(n)
+ */
+
+
 package Recursion;
 
 import java.util.ArrayList;
