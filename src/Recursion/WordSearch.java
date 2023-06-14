2784c8a2c42f163cd558b6b2adc344cb61e98038
Sayantan <sayantan.das208@gmail.com>
2023-06-14 22:17:39 +0530

    Word Search

--- src/Recursion/WordSearch.java
+++ src/Recursion/WordSearch.java
@@ -1,3 +1,30 @@
+/*
+Given an m x n grid of characters board and a string word, return true if word exists in the grid.
+The word can be constructed from letters of sequentially adjacent cells,
+where adjacent cells are horizontally or vertically neighboring.
+The same letter cell may not be used more than once.
+https://leetcode.com/problems/word-search/description/
+
+Approach:
+The approach used in the given solution is Depth First Search (DFS) algorithm.
+The solution iterates over each cell of the input matrix and calls the dfs function
+to check if the given word can be formed starting from that cell.
+The dfs function searches for the next character of the word in the neighbouring cells (up, down, left, right) and
+marks the current cell as visited by changing the value to '#' to avoid revisiting the same cell.
+If the entire word is found in the matrix, the function returns true;
+otherwise, it backtracks by restoring the value of the current cell and returns false.
+
+TC: O(mn4^k), where m and n are the number of rows and columns in the input matrix,
+and k is the length of the given word. This is because the solution iterates over each cell of the matrix,
+and for each cell, it makes at most four recursive calls, one for each neighbouring cell.
+The worst-case time complexity occurs when the word is not found, and the DFS algorithm traverses all possible paths,
+which can be up to 4^k paths.
+SC: O(k), where k is the length of the given word.
+This is because the solution uses the call stack to store the state of the recursive calls,
+and the maximum depth of the call stack is k.
+ */
+
+
 package Recursion;
 
 public class WordSearch {
