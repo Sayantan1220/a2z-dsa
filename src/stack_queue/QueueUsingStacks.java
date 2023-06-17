3633c55830a69cd1f421f9c141f65ac12994cc12
Sayantan <sayantan.das208@gmail.com>
2023-06-17 20:33:08 +0530

    Implement queue using stack

--- src/stack_queue/QueueUsingStacks.java
+++ src/stack_queue/QueueUsingStacks.java
@@ -11,6 +11,7 @@ You must use only standard operations of a stack, which means only push to top,
 peek/pop from top, size, and is empty operations are valid.
 Depending on your language, the stack may not be supported natively.
 You may simulate a stack using a list or deque (double-ended queue) as long as you use only a stack's standard operations.
+https://leetcode.com/problems/implement-queue-using-stacks/
 
 TC: Push - O(1)
     Pop - O(n)
