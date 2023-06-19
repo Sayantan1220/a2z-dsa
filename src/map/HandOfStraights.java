247b2cae3c443b52d2ed6061cdbe603512a5d27c
Sayantan <sayantan.das208@gmail.com>
2023-06-19 20:21:37 +0530

    Hands of Straights

--- src/map/HandOfStraights.java
+++ src/map/HandOfStraights.java
@@ -7,24 +7,21 @@ import java.util.Map;
 
 public class HandOfStraights {
 
-    public boolean isNStraightHand(int[] hand, int groupSize) {
+    public static boolean isNStraightHand(int[] hand, int groupSize) {
         Map<Integer, Integer> countMap = new HashMap<>();
         for (int card : hand) {
             countMap.put(card, countMap.getOrDefault(card, 0) + 1);
         }
         Arrays.sort(hand);
-        for (int i = 0; i < hand.length; i++) {
-            if (countMap.get(hand[i]) == 0) {
+        for (int k : hand) {
+            if (countMap.get(k) == 0) {
                 continue;
             }
-
             for (int j = 0; j < groupSize; j++) {
-                int currCard = hand[i] + j;
-
+                int currCard = k + j;
                 if (countMap.getOrDefault(currCard, 0) == 0) {
                     return false;
                 }
-
                 countMap.put(currCard, countMap.get(currCard) - 1);
             }
         }
@@ -33,7 +30,7 @@ public class HandOfStraights {
 
     public static void main(String[] args) {
         String s = "239";
-        List<String> result = letterCombinations(s);
+        List<String> result = isNStraightHand(s);
         System.out.println(result);
     }
 }
