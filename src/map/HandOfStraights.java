38ef9da89b46169dd5002eee5928849bf64b292c
Sayantan <sayantan.das208@gmail.com>
2023-06-19 21:15:28 +0530

    Hands of Straights

--- src/map/HandOfStraights.java
+++ src/map/HandOfStraights.java
@@ -1,8 +1,36 @@
+/*
+Alice has some number of cards and she wants to rearrange the cards into groups so that each group is of size groupSize,
+and consists of groupSize consecutive cards.
+Given an integer array hand where hand[i] is the value written on the ith card and an integer groupSize,
+return true if she can rearrange the cards, or false otherwise.
+https://leetcode.com/problems/hand-of-straights/description/
+
+Approach:
+Create a map countMap to count the frequency of each card in the hand.
+Sort the hand in ascending order.
+Iterate through the hand and for each card:
+a. If the frequency of the current card is 0, continue to the next card.
+b. For each number in a group of size groupSize starting from the current card:
+i. If the frequency of the number is 0, return false as it is not possible to form the required group.
+ii. Decrement the frequency of the number in the countMap.
+If we have successfully formed all groups, return true.
+
+Complexity:
+Time Complexity:
+The time complexity of the solution is O(nlogn) where n is the size of the hand.
+This is because we sort the hand before iterating through it, which takes O(nlogn) time.
+The subsequent iterations take O(n) time as we iterate through each card once.
+
+Space Complexity:
+The space complexity of the solution is O(n) as we use a map to store the frequency of each card in the hand.
+In the worst case, if all cards in the hand are unique, the map will have n entries.
+*/
+
+
 package map;
 
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class HandOfStraights {
@@ -29,8 +57,8 @@ public class HandOfStraights {
     }
 
     public static void main(String[] args) {
-        String s = "239";
-        List<String> result = isNStraightHand(s);
+        int[] arr = {1,2,3,6,2,3,4,7,8};
+        boolean result = isNStraightHand(arr, 3);
         System.out.println(result);
     }
 }
