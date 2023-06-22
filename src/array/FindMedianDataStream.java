359e035f1986cfa181207dccfc5ed3521c91754f
Sayantan <sayantan.das208@gmail.com>
2023-06-22 23:34:44 +0530

    Find median from data stream

--- src/array/FindMedianDataStream.java
+++ src/array/FindMedianDataStream.java
@@ -1,3 +1,27 @@
+/*
+The median is the middle value in an ordered integer list.
+If the size of the list is even, there is no middle value, and the median is the mean of the two middle values.
+For example, for arr = [2,3,4], the median is 3.
+For example, for arr = [2,3], the median is (2 + 3) / 2 = 2.5.
+Implement the MedianFinder class:
+MedianFinder() initializes the MedianFinder object.
+void addNum(int num) adds the integer num from the data stream to the data structure.
+double findMedian() returns the median of all elements so far. Answers within 10-5 of the actual answer will be accepted.
+https://leetcode.com/problems/find-median-from-data-stream/description/
+
+Approach:
+In addNum method,
+keep adding the incoming elements and at the time of adding itself insert them in their correct place such that they are in ascending order. (See the code)
+
+In the findMedian method,
+Return the middle element if the size of the list is odd,
+Else return the average of middle two elements.
+
+TC:O(n^2)
+SC:O(n)
+ */
+
+
 package array;
 
 import java.util.ArrayList;
