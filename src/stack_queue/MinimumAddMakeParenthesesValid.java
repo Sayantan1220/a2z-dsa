f3cea44a2fdc6401e04aa7383d4de30754cb1cf1
Sayantan <sayantan.das208@gmail.com>
2023-06-24 20:59:57 +0530

    Minimum Add to Make Parentheses Valid

--- src/stack_queue/MinimumAddMakeParenthesesValid.java
+++ src/stack_queue/MinimumAddMakeParenthesesValid.java
@@ -1,5 +1,25 @@
 package stack_queue;
 
 public class MinimumAddMakeParenthesesValid {
+    public int minAddToMakeValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        int count = 0;
+        for(char c : s.toCharArray()) {
+            if(stack.isEmpty()) {
+                if(c == '(') stack.push(c);
+                else count++;
+            }
+            else {
+                if(c=='(') stack.push(c);
+                else {
+                    stack.pop();
+                }
+            }
+        }
+        if(!stack.isEmpty() && stack.peek() == '(') {
+            count += stack.size();
+        }
+        return count;
+    }
 
 }
