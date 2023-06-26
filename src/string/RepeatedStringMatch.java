98fef392df6775ee1b9f0371f1388c578c22c5ed
Sayantan <sayantan.das208@gmail.com>
2023-06-26 22:33:08 +0530

    Repeated String Match

--- src/string/RepeatedStringMatch.java
+++ src/string/RepeatedStringMatch.java
@@ -1,3 +1,13 @@
+/*
+Given two strings a and b, return the minimum number of times you should repeat string a so that string b is a substring of it.
+If it is impossible for b to be a substring of a after repeating it, return -1.
+Notice: string "abc" repeated 0 times is "", repeated 1 time is "abc" and repeated 2 times is "abcabc".
+https://leetcode.com/problems/repeated-string-match/description/
+
+TC: O(n)
+SC: O(n)
+ */
+
 package string;
 
 public class RepeatedStringMatch {
