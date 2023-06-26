b17fb22da3aff471af2ae05b357d8534ebb382bd
Sayantan <sayantan.das208@gmail.com>
2023-06-26 22:30:06 +0530

    Repeated String Match

--- src/string/RepeatedStringMatch.java
+++ src/string/RepeatedStringMatch.java
@@ -1,4 +1,18 @@
 package string;
 
 public class RepeatedStringMatch {
+
+    public int repeatedStringMatch(String a, String b) {
+        String copy = "";
+        int count=0;
+        while(copy.length()<b.length()){
+            copy+=a;
+            count++;
+        }
+        if(copy.indexOf(b)>=0)
+            return count;
+        if((copy+a).indexOf(b)>=0)
+            return ++count;
+        return -1;
+    }
 }
