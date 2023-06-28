ba256459c7c96eb0d7580ce9c786e4c01c431aba
Sayantan <sayantan.das208@gmail.com>
2023-06-28 22:54:52 +0530

    Minimum Window Substring

--- src/string/MinimumWindowSubstring.java
+++ src/string/MinimumWindowSubstring.java
@@ -1,3 +1,36 @@
+/*
+Given two strings s and t of lengths m and n respectively, return the minimum window
+substring of s such that every character in t (including duplicates) is included in the window.
+If there is no such substring, return the empty string "".
+The testcases will be generated such that the answer is unique.
+https://leetcode.com/problems/minimum-window-substring/description/
+
+Approach
+i is used to take character and j is used to release characters.
+
+Let's add all occurances of character in t.
+for(int i = 0; i < t.length(); i++){
+    char ch = t.charAt(i);
+    hm2.put(ch, hm2.getOrDefault(ch, 0) + 1);
+}
+Now lets take two pointer i and j and count variable mcnt.
+So I will increase my count if my HashMap of t(count of particular variable) is greater than HashMap of s(count of that particular variable).
+char ch = s.charAt(i);
+if(hm1.getOrDefault(ch, 0) <= hm2.getOrDefault(ch, 0)){
+    mcnt++;
+}
+After this part now it is time to calculate the mniimum one.
+So, I will make a String potential answer(pans) and the substring will be from j + 1 to i + 1.
+if pans.length is smaller than answer.length then it means It has the minimum length so I will store it in ans. And I will increase j to find smaller.
+6)Now it is time to release character so we will check if frequency is 1 then directly remove from HashMap else decrease the frequency by one.
+if(hm1.get(ch) == 1) hm1.remove(ch);
+else hm1.put(ch, hm1.get(ch) - 1);
+Now check if HashMap for s is lesser than hashmap for t then decrease count. It means we can still get a lesser length of string.
+
+TC: O(n)
+SC: O(n)
+ */
+
 package string;
 
 import java.util.HashMap;
