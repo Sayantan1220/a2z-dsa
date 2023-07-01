5ae3748791cf563d8aeeeb6992efe11521e0ce16
Sayantan <sayantan.das208@gmail.com>
2023-07-01 19:43:32 +0530

    Assign Cookies

--- src/array/AssignCookies.java
+++ src/array/AssignCookies.java
@@ -1,3 +1,32 @@
+/*
+Assume you are an awesome parent and want to give your children some cookies.
+But, you should give each child at most one cookie.
+Each child i has a greed factor g[i], which is the minimum size of a cookie that the child will be content with;
+and each cookie j has a size s[j]. If s[j] >= g[i], we can assign the cookie j to the child i, and the child i will be content.
+Your goal is to maximize the number of your content children and output the maximum number.
+https://leetcode.com/problems/assign-cookies/description/
+
+Approach
+Greedy Algorithm:
+Sort the greed factors of children and the sizes of cookies in non-decreasing order.
+Initialize a variable contentChildren to 0 to keep track of the number of children that can be content with a cookie.
+Use two pointers i and j to point to the current child's greed factor and the current cookie size, respectively.
+Iterate over the two sorted arrays, and for each cookie size, check if it can satisfy the current child's greed factor. If so, assign the cookie to the child, increment contentChildren and move on to the next child and cookie. If not, move on to the next cookie.
+Return contentChildren.
+
+Complexity:
+Time Complexity:
+Both approaches involve sorting the arrays, which takes O(n log n) time.
+Then we iterate over the arrays, which takes O(n) time.
+Using a priority_queue adds an additional O(log n) time per insertion and removal,
+so the max heap approach has a slightly higher time complexity. Therefore, the time complexity of both approaches is O(n log n).
+
+Space Complexity:
+Both approaches use O(1) extra space for the pointers and variables.
+The max heap approach uses O(n) extra space for the priority_queue.
+Therefore the space complexity of the greedy algorithm is O(1) and the space complexity of the max heap algorithm is O(n).
+ */
+
 package array;
 
 import java.util.Arrays;
