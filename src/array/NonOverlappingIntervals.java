917fb28c4e6f2b5cb8dafd30dba70f0f52ea6e50
Sayantan <sayantan.das208@gmail.com>
2023-07-04 21:40:37 +0530

    Non-overlapping Intervals

--- src/array/NonOverlappingIntervals.java
+++ src/array/NonOverlappingIntervals.java
@@ -1,3 +1,23 @@
+/*
+Given an array of intervals where intervals[i] = [starti, endi],
+return the minimum number of intervals you need to remove to make the rest of the intervals non-overlapping.
+
+https://leetcode.com/problems/non-overlapping-intervals/description/
+
+Approach
+So first array is not null so there will be always one solution.
+Now sort the 2nd column of this array to apply greedy approach.
+to sort 2nd column lets use Comparator.
+Arrays.sort(nums, Comparator.comparingDouble(o -> o[1]));
+o[1] denotes 2nd column to be sorted.
+now check for valid intervals. if nextStart >= prevLast then it is valid.
+So, we are incrementing count and assign prevLast to newLast.
+
+TC: O(n)
+SC: O(1)
+ */
+
+
 package array;
 
 import java.util.Arrays;
