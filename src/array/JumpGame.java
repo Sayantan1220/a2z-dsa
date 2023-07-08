b07c397644c83e77dc5cab41bf9ab63757335fb4
Sayantan <sayantan.das208@gmail.com>
2023-07-08 22:39:15 +0530

    Jump Game

--- src/array/JumpGame.java
+++ src/array/JumpGame.java
@@ -1,7 +1,7 @@
 package array;
 
 public class JumpGame {
-    public boolean canJump(int[] nums) {
+    public static boolean canJump(int[] nums) {
         int last = nums.length - 1;
         int maxReach = 0; // Maximum index we can reach
 
@@ -13,5 +13,11 @@ public class JumpGame {
             }
         }
         return false; // Cannot reach the last index
+    }
+
+    public static void main(String[] args) {
+        int[] bills = {1,0,2};
+        boolean result = canJump(bills);
+        System.out.println(result);
     }
 }
