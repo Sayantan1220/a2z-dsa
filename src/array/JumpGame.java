0db2aa101f78772787643ae6a94e66bf2e1b336b
Sayantan <sayantan.das208@gmail.com>
2023-07-08 22:41:25 +0530

    Jump Game

--- src/array/JumpGame.java
+++ src/array/JumpGame.java
@@ -1,3 +1,24 @@
+/*
+You are given an integer array nums.
+You are initially positioned at the array's first index,
+and each element in the array represents your maximum jump length at that position.
+Return true if you can reach the last index, or false otherwise.
+https://leetcode.com/problems/jump-game/description/
+
+Approach
+Initialize the variable last as the index of the last element in the array (nums.length - 1).
+Initialize the variable maxReach as 0, representing the maximum index we can currently reach from the starting position.
+Iterate through the array nums from left to right using the loop variable i.
+Update maxReach using the expression Math.max(maxReach, i + nums[i]).
+This calculates the maximum index we can reach by taking a jump from the current position i.
+If maxReach ever reaches or exceeds the last index, return true as we can reach the end.
+If the loop finishes without returning true, it means we cannot reach the last index. In this case, return false.
+Complexity
+
+TC:O(N)
+SC:O(1)
+ */
+
 package array;
 
 public class JumpGame {
@@ -7,7 +28,6 @@ public class JumpGame {
 
         for (int i = 0; i <= maxReach; i++) {
             maxReach = Math.max(maxReach, i + nums[i]);
-
             if (maxReach >= last) {
                 return true; // We can reach the last index
             }
@@ -16,7 +36,7 @@ public class JumpGame {
     }
 
     public static void main(String[] args) {
-        int[] bills = {1,0,2};
+        int[] bills = {2,3,1,1,4};
         boolean result = canJump(bills);
         System.out.println(result);
     }
