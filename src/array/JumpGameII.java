21dc34302613e6cbcef3d3ad9af511a267b782cd
Sayantan <sayantan.das208@gmail.com>
2023-07-11 19:36:13 +0530

    Jump Game II

--- src/array/JumpGameII.java
+++ src/array/JumpGameII.java
@@ -1,4 +1,15 @@
 package array;
 
 public class JumpGameII {
+    public int jump(int[] nums) {
+        int jumps = 0, curEnd = 0, curFarthest = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            curFarthest = Math.max(curFarthest, i + nums[i]);
+            if (i == curEnd) {
+                jumps++;
+                curEnd = curFarthest;
+            }
+        }
+        return jumps;
+    }
 }
