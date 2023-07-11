237699088174a03fa62d1cfe6bbec25ab8db7531
Sayantan <sayantan.das208@gmail.com>
2023-07-11 19:39:03 +0530

    Jump Game II

--- src/array/JumpGameII.java
+++ src/array/JumpGameII.java
@@ -1,3 +1,23 @@
+/*
+You are given a 0-indexed array of integers nums of length n. You are initially positioned at nums[0].
+Each element nums[i] represents the maximum length of a forward jump from index i. In other words,
+if you are at nums[i], you can jump to any nums[i + j] where:
+0 <= j <= nums[i] and
+i + j < n
+Return the minimum number of jumps to reach nums[n - 1]. The test cases are generated such that you can reach nums[n - 1].
+https://leetcode.com/problems/jump-game-ii/description/
+
+Approach:
+The main idea is based on greedy.
+Let's say the range of the current jump is [curBegin, curEnd],
+curFarthest is the farthest point that all points in [curBegin, curEnd] can reach.
+Once the current point reaches curEnd, then trigger another jump,
+and set the new curEnd with curFarthest, then keep the above steps
+
+TC:O(N)
+SC:O(1)
+ */
+
 package array;
 
 public class JumpGameII {
