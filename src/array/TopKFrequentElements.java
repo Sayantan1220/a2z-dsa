52c788977d2455332de36c059a49e599679cb2a7
Sayantan <sayantan.das208@gmail.com>
2023-07-12 23:37:22 +0530

    Top K Frequent Elements

--- src/array/TopKFrequentElements.java
+++ src/array/TopKFrequentElements.java
@@ -1,4 +1,32 @@
 package array;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+
 public class TopKFrequentElements {
+    public int[] topKFrequent(int[] nums, int k) {
+        // Count the frequency of each element
+        Map<Integer, Integer> frequencyMap = new HashMap<>();
+        for (int num : nums) {
+            frequencyMap.put(num, frequencyMap.getOrDefault(num, 0) + 1);
+        }
+
+        // Create a min heap (PriorityQueue) to store the k most frequent elements
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>((a, b) -> frequencyMap.get(a) - frequencyMap.get(b));
+        for (int num : frequencyMap.keySet()) {
+            minHeap.offer(num);
+            if (minHeap.size() > k) {
+                minHeap.poll();
+            }
+        }
+
+        // Build the result array
+        int[] result = new int[k];
+        for (int i = k - 1; i >= 0; i--) {
+            result[i] = minHeap.poll();
+        }
+
+        return result;
+    }
 }
