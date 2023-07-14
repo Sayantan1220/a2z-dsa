0a0c5bad3974b1d289ef6da664774ff4f461e4d2
Sayantan <sayantan.das208@gmail.com>
2023-07-14 17:02:23 +0530

    Longest Happy Prefix

--- src/string/LongestHappyPrefix.java
+++ src/string/LongestHappyPrefix.java
@@ -1,5 +1,25 @@
 package string;
 
 public class LongestHappyPrefix {
+    public String longestPrefix(String s) {
+        int n = s.length();
+        int[] lps = new int[s.length()];
+        int i = 1, len = 0;
+        while (i < n) {
+            if (s.charAt(i) == s.charAt(len)) {
+                len++;
+                lps[i] = len;
+                i++;
+            } else {
+                if (len > 0) {
+                    len = lps[len - 1];
+                } else {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+        return s.substring(0, lps[s.length() - 1]);
+    }
 
 }
