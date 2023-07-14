1e3a3cfdfb2f10ad1ef1e268e473653b0428ad17
Sayantan <sayantan.das208@gmail.com>
2023-07-14 17:08:03 +0530

    Longest Happy Prefix

--- src/string/LongestHappyPrefix.java
+++ src/string/LongestHappyPrefix.java
@@ -1,3 +1,39 @@
+/*
+A string is called a happy prefix if is a non-empty prefix which is also a suffix (excluding itself).
+Given a string s, return the longest happy prefix of s. Return an empty string "" if no such prefix exists.
+https://leetcode.com/problems/longest-happy-prefix/description/
+
+Intuition
+To solve this problem, we can use the Rabin-Karp algorithm.
+This algorithm uses hash values to compare strings in constant time,
+allowing us to compare substrings of the given string with the same length as the pattern in constant time.
+We can then slide a window of length m (length of the pattern) over the string
+and check if the hash value of the window matches the hash value of the pattern.
+If they match, we can compare the substring with the pattern to check for a match.
+
+Approach
+First, we need to compute the hash value of the pattern and the first window of length m in the string.
+Then, we can slide the window over the string, computing the hash value of each new window using the previous hash value
+and the characters at the start and end of the window. If the hash values match, we compare the substring
+with the pattern to check for a match. If we find a match, we can return the substring.
+
+To compute the hash value, we can use a rolling hash function that takes the previous hash value,
+the first character in the window, and the last character in the window as input.
+The hash function should have the property that it can be computed in constant time,
+and it should have a low probability of collisions (two different strings having the same hash value).
+
+Complexity
+Time complexity : O(nm)
+The Rabin-Karp algorithm has a time complexity of O(n + m) in the average case,
+where n is the length of the string and m is the length of the pattern.
+The worst-case time complexity is O(nm), which occurs when there are many hash collisions.
+However, this is rare in practice, and the average-case time complexity is much faster
+than naive string matching algorithms that have a time complexity of O(nm).
+
+Space complexity : O(1)
+The space complexity of the algorithm is O(1), as we only need to store the hash value of the pattern and the current window.
+ */
+
 package string;
 
 public class LongestHappyPrefix {
