430d9729fa15329174569f731a5068e0d469141a
Sayantan <sayantan.das208@gmail.com>
2023-07-19 23:20:59 +0530

    Same Tree

--- src/binary_tree/SameTree.java
+++ src/binary_tree/SameTree.java
@@ -1,3 +1,36 @@
+/*
+Given the roots of two binary trees p and q, write a function to check if they are the same or not.
+Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.
+https://leetcode.com/problems/same-tree/description/
+
+Intuition
+The intuition behind the solution is to recursively check if two binary trees are identical.
+If both trees are empty (null), they are considered identical.
+If only one tree is empty or the values of the current nodes are different, the trees are not identical.
+Otherwise, we recursively check if the left and right subtrees of both trees are identical.
+
+Approach
+Check the base case: if both trees are null, return true.
+Check if only one tree is null or the values of the current nodes are different, return false.
+Recursively check if the left subtrees of both trees are identical.
+Recursively check if the right subtrees of both trees are identical.
+Return the logical AND of the results from steps 3 and 4.
+
+Complexity
+Time complexity:
+The time complexity of the solution is O(min(N,M))O(min(N, M))O(min(N,M)),
+where N and M are the number of nodes in the two trees, respectively.
+This is because we need to visit each node once in order to compare their values.
+In the worst case, where both trees have the same number of nodes, the time complexity would be O(N).
+
+Space complexity:
+The space complexity of the solution isO(min(H1,H2))O(min(H1, H2))O(min(H1,H2)),
+where H1 and H2 are the heights of the two trees, respectively.
+This is because the space used by the recursive stack is determined by the height of the smaller tree.
+In the worst case, where one tree is significantly larger than the other,
+the space complexity would be closer to O(N) or O(M), depending on which tree is larger.
+ */
+
 package binary_tree;
 
 public class SameTree {
