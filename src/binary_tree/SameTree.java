df008374e158670126aeb3b59e8828be890d68d8
Sayantan <sayantan.das208@gmail.com>
2023-07-19 23:18:42 +0530

    Same Tree

--- src/binary_tree/SameTree.java
+++ src/binary_tree/SameTree.java
@@ -2,6 +2,19 @@ package binary_tree;
 
 public class SameTree {
 
+    public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+    }
+
     public boolean isSameTree(TreeNode p, TreeNode q) {
         // Base case: if both trees are null, they are identical
         if (p == null && q == null) {
