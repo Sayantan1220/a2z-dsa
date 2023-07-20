be893a0f6842708748dc84ce5a88084a5f08bcb1
Sayantan <sayantan.das208@gmail.com>
2023-07-20 23:46:06 +0530

    Binary Tree Zigzag Level Order Traversal

--- src/binary_tree/BinaryTreeZigzagLevelOrderTraversal.java
+++ src/binary_tree/BinaryTreeZigzagLevelOrderTraversal.java
@@ -2,6 +2,19 @@ package binary_tree;
 
 public class BinaryTreeZigzagLevelOrderTraversal {
 
+    public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
 
         List<List<Integer>> zlist = new ArrayList<>();
