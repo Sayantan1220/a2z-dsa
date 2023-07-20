ac804d6c57e9451fd53fdb5329747f156b3b8efb
Sayantan <sayantan.das208@gmail.com>
2023-07-20 23:48:31 +0530

    Binary Tree Zigzag Level Order Traversal

--- src/binary_tree/BinaryTreeZigzagLevelOrderTraversal.java
+++ src/binary_tree/BinaryTreeZigzagLevelOrderTraversal.java
@@ -1,3 +1,12 @@
+/*
+Given the root of a binary tree, return the zigzag level order traversal of its nodes' values.
+(i.e., from left to right, then right to left for the next level and alternate between).
+https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/description/
+
+Time complexity: O(n)
+Space complexity:O(n)
+ */
+
 package binary_tree;
 
 import java.util.*;
