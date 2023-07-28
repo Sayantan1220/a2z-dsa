d0f836e0cdac495fca4cbc6a8d76cb553e0235cf
Sayantan <sayantan.das208@gmail.com>
2023-07-28 23:13:05 +0530

    Lowest Common Ancestor of a Binary Tree

--- src/binary_tree/LowestCommonAncestorBinaryTree.java
+++ src/binary_tree/LowestCommonAncestorBinaryTree.java
@@ -1,4 +1,21 @@
 package binary_tree;
 
 public class LowestCommonAncestorBinaryTree {
+
+    public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode(int x) { val = x; }
+   }
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root == null || root == p || root == q) return root;
+        TreeNode forLeft = lowestCommonAncestor(root.left,p,p);
+        TreeNode forRight = lowestCommonAncestor(root.right,p,q);
+        if(forLeft!= null && forRight!=null) return root;
+        else if(forLeft!=null) return forLeft;
+        else if(forRight!=null) return forRight;
+        else return null;
+    }
 }
