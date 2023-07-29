bacb4d0761e32d924ffee65968de5af4dcdcfd52
Sayantan <sayantan.das208@gmail.com>
2023-07-29 07:00:00 +0530

    Maximum Width of Binary Tree

--- src/binary_tree/MaximumWidthBinaryTree.java
+++ src/binary_tree/MaximumWidthBinaryTree.java
@@ -1,4 +1,20 @@
 package binary_tree;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MaximumWidthBinaryTree {
+
+    Map<Integer, Integer> levelMin = new HashMap<>();
+    public int widthOfBinaryTree(TreeNode root) {
+        return dfs(root, 1, 1);
+    }
+
+    private int dfs(TreeNode node, int depth, int index) {
+        if (node == null) {
+            return 0;
+        }
+        levelMin.putIfAbsent(depth, index);
+        return Math.max(index - levelMin.get(depth) + 1, Math.max(dfs(node.left, depth + 1, index * 2), dfs(node.right, depth + 1, index * 2 + 1)));
+    }
 }
