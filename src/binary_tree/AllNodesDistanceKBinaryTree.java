9dd08e03662d656ede23917f6c20c21f2e89d1e3
Sayantan <sayantan.das208@gmail.com>
2023-07-30 23:32:07 +0530

    All Nodes Distance K in Binary Tree

--- src/binary_tree/AllNodesDistanceKBinaryTree.java
+++ src/binary_tree/AllNodesDistanceKBinaryTree.java
@@ -4,6 +4,25 @@ return an array of the values of all nodes that have a distance k from the targe
 You can return the answer in any order.
 https://leetcode.com/problems/all-nodes-distance-k-in-binary-tree/description/
 
+Approach :
+First, create an unordered map parent to store the parent node of each node in the binary tree.
+Use the getparentbyDFS or getparentbyBFS function to populate the parent map by performing a depth-first traversal or bredth first search of the binary tree.
+Initialize a queue q and an unordered map vis to keep track of visited nodes.
+Mark the target node as visited by setting vis[target] to true and push it into the queue.
+Perform a BFS traversal while the queue is not empty:
+a. Retrieve the front node curr from the queue.
+b. If the current node's left child exists and has not been visited, mark it as visited, and push it into the queue.
+c. If the current node's right child exists and has not been visited, mark it as visited, and push it into the queue.
+d. If the parent of the current node exists and has not been visited, mark it as visited, and push it into the queue.
+e. Repeat steps a to d for all nodes at the current level.
+f. Increment the level lev by 1.
+g. If the current level is equal to k, exit the traversal.
+Store the nodes in the queue in a vector ans.
+Return the vector ans, which contains all the nodes at a distance of k from the target node.
+Complexity
+
+TC:O(n)
+SC:O(n)
  */
 
 package binary_tree;
