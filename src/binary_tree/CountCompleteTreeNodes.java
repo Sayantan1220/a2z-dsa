be4c547877539d31d19b5baabd0fe7925718edc0
Sayantan <sayantan.das208@gmail.com>
2023-07-31 23:42:52 +0530

    Count Complete Tree Nodes

--- src/binary_tree/CountCompleteTreeNodes.java
+++ src/binary_tree/CountCompleteTreeNodes.java
@@ -1,4 +1,19 @@
 package binary_tree;
 
 public class CountCompleteTreeNodes {
+
+    public int countNodes(TreeNode root) {
+        if(root == null) return 0;
+
+        int ans = countAllNodes(root);
+        return ans;
+    }
+    int countAllNodes(TreeNode root)
+    {
+        if(root == null) return 0;
+        int lh =  countAllNodes(root.left);
+        int rh =  countAllNodes(root.right);
+
+        return lh + rh + 1;
+    }
 }
