8c3e28e992271f535a268c210213404f75af6974
Sayantan <sayantan.das208@gmail.com>
2023-07-31 23:45:23 +0530

    Count Complete Tree Nodes

--- src/binary_tree/CountCompleteTreeNodes.java
+++ src/binary_tree/CountCompleteTreeNodes.java
@@ -5,6 +5,22 @@ and all nodes in the last level are as far left as possible.
 It can have between 1 and 2h nodes inclusive at the last level h.
 Design an algorithm that runs in less than O(n) time complexity.
 https://leetcode.com/problems/count-complete-tree-nodes/description/
+
+Intuition
+Using resursion we can reach each of the node and add the nodes.
+similar to finding the height of the tree (helped me atleast).
+
+Approach
+call the left nodes till it becomes null which will return 0.
+now you reach to the node where left child is null.
+check for the right child
+if it is not null then repeat the recursive process.
+else it is also null means we got the leaf node.
+we use lh + rh + 1 so that lh and rh helps in returning the result of children to the parent
+and adding 1 means adding the parent's count.
+
+TC:O(n)
+SC:O(n)
  */
 
 package binary_tree;
