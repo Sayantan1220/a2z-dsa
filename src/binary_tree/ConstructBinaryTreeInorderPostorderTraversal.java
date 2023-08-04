189c5318bc98b2778e4007b82bb81f2f3c6cefbd
Sayantan <sayantan.das208@gmail.com>
2023-08-04 18:54:47 +0530

    Construct Binary Tree from Inorder and Postorder Traversal

--- src/binary_tree/ConstructBinaryTreeInorderPostorderTraversal.java
+++ src/binary_tree/ConstructBinaryTreeInorderPostorderTraversal.java
@@ -1,3 +1,36 @@
+/*
+Given two integer arrays inorder and postorder where inorder is the inorder traversal of a binary tree
+and postorder is the postorder traversal of the same tree, construct and return the binary tree.
+https://leetcode.com/problems/construct-binary-tree-from-inorder-and-postorder-traversal/description/
+
+Intuition
+Basic idea of a binary tree is to have a root, left node and right node.
+Inorder traversal represents the array in which all thel elements of the left subtree are towards left side of the root
+and right subtree towards the right side of the root
+Postfix traversal always has the root element of the subtree towards the end. That means all the elements
+towards the right can either be nodes at same level or at parent levels.
+
+Approach
+Calculation of the split point
+Since we know that root element is always the last in postfix, we can get the position of the root from inorder accordingly.
+This will act as the split point of the sub tree
+
+Calculate the left and right sub tree
+We have already established the condition that elements on the left side of the inorder are left sub tre
+and right side as right sub tree.
+How do we determine the sub array from postfix?
+2.1. As we know postfix is represented as left-right-root that means number of elements in the left section will be
+same as the number of elements in towards the left sub array of the root in inorder.
+2.2. Similarly remaining element in the sub array except the last one represents the right sub array
+Repeat the process recursively which each sub-tree/sub-array
+
+Optimisations
+To increase the speed of index search from inorder traversal, indexes for the elements can be cached in a map.
+
+TC:O(n)
+SC:O(n)
+ */
+
 package binary_tree;
 
 import java.util.HashMap;
