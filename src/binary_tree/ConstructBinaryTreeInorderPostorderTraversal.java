bbfce4764e9ea315856662ff118c08f47ef477ac
Sayantan <sayantan.das208@gmail.com>
2023-08-04 18:52:18 +0530

    Construct Binary Tree from Inorder and Postorder Traversal

--- src/binary_tree/ConstructBinaryTreeInorderPostorderTraversal.java
+++ src/binary_tree/ConstructBinaryTreeInorderPostorderTraversal.java
@@ -1,6 +1,20 @@
 package binary_tree;
 
 public class ConstructBinaryTreeInorderPostorderTraversal {
+
+    public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+    }
+
     public TreeNode buildTree(int[] inorder, int[] postorder) {
         final var length = postorder.length;
         final var map = new HashMap<Integer, Integer>();
