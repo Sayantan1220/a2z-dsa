d40427b11a0b3b2be0357e03af80c92af62e4071
Sayantan <sayantan.das208@gmail.com>
2023-08-13 20:10:44 +0530

    Shortest Path in Binary Matrix

--- src/graph/ShortestPathBinaryMatrix.java
+++ src/graph/ShortestPathBinaryMatrix.java
@@ -1,3 +1,33 @@
+/*
+
+Intuition
+The goal is to find the shortest clear path from the top-left cell to the bottom-right cell in the binary matrix.
+A clear path consists of cells with a value of 0, and adjacent cells are considered 8-directionally connected.
+The BFS algorithm is used to explore cells in a breadth-first manner, starting from the top-left cell and moving towards the bottom-right cell.
+Approach
+The algorithm starts by checking if the top-left and bottom-right cells are obstacles (1). If either of them is an obstacle, there is no clear path, and -1 is returned.
+A directions array is defined to represent the 8 possible directions to explore neighboring cells.
+A queue is initialized, and the top-left cell is added to the queue as the starting point.
+The grid is updated to mark the starting cell as visited.
+A pathLength variable is initialized to track the length of the path.
+The algorithm enters a while loop until the queue becomes empty:
+The size of the current level is obtained from the queue.
+For each cell in the current level:
+If the cell is the bottom-right cell, the pathLength is returned as the shortest clear path.
+For each direction in the directions array:
+The neighboring cell coordinates are calculated based on the current cell and the direction.
+If the neighboring cell is within the grid boundaries, has a value of 0, and hasn't been visited:
+The neighboring cell is added to the queue.
+The neighboring cell is marked as visited in the grid.
+The pathLength is incremented to move to the next level.
+Complexity
+Time complexity:
+The time complexity of the BFS algorithm is O(V + E), where V is the number of vertices (cells) and E is the number of edges (adjacent connections). In this case, V represents the number of cells in the grid, which is n^2, and E is proportional to V, as each cell has at most 8 neighbors in the 8-directional connectivity. Therefore, the time complexity is O(n^2).
+
+Space complexity:
+The space complexity is determined by the queue and the grid. The queue can store at most n^2 cells in the worst case, resulting in O(n^2) space. The grid uses O(n^2) space as well to represent the visited status of each cell. Hence, the overall space complexity is O(n^2).
+ */
+
 package graph;
 
 import java.util.LinkedList;
