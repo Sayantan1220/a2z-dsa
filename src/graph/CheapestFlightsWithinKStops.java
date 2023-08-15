2d4c555fb3d0c8cbf40eff740f2cd734f8d51be5
Sayantan <sayantan.das208@gmail.com>
2023-08-15 21:41:11 +0530

    Cheapest Flights Within K Stops

--- src/graph/CheapestFlightsWithinKStops.java
+++ src/graph/CheapestFlightsWithinKStops.java
@@ -1,3 +1,14 @@
+/*
+There are n cities connected by some number of flights. You are given an array flights where flights[i] =
+[fromi, toi, pricei] indicates that there is a flight from city fromi to city toi with cost pricei.
+You are also given three integers src, dst, and k, return the cheapest price from src to dst with at most k stops.
+If there is no such route, return -1.
+https://leetcode.com/problems/cheapest-flights-within-k-stops/description/
+
+TC : O(V+E∗K)
+SC : O(V+E∗K)
+ */
+
 package graph;
 
 import java.util.*;
