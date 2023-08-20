d6d16f29b48faecb347ff2c87c50a1625d8119bd
Sayantan <sayantan.das208@gmail.com>
2023-08-20 14:55:35 +0530

    Number of Operations to Make Network Connected

--- src/graph/NumberOperationsMakeNetworkConnected.java
+++ src/graph/NumberOperationsMakeNetworkConnected.java
@@ -7,6 +7,19 @@ connected computers, and place them between any pair of disconnected computers t
 Return the minimum number of times you need to do this in order to make all the computers connected.
 If it is not possible, return -1.
 https://leetcode.com/problems/number-of-operations-to-make-network-connected/description/
+
+Approach:
+The basic understanding to connect n nodes requires a minimum of n-1 edges.
+In case of multiple groups of nodes we might need more nodes as they may be in tree like data structure.
+Using the above approach we have to see whether n-1 edges are available.
+And to get the multiple group of nodes we use union find.
+
+1. Initially the parent for all the nodes will the node itself
+2. If two nodes are connected then we use Union method to join the nodes with parent of second node as first node
+3. Find the parent of the node by looking into the parent array until you reach the node where
+the parent of the node is the node itself
+4. Finally count the number of groups and not connected nodes by finding the number of parent nodes with unique values.
+5. Return the count-1.
  */
 
 package graph;
