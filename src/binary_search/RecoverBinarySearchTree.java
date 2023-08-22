657eea1fb6eb695ae0275710744168063f6d40dd
Sayantan <sayantan.das208@gmail.com>
2023-08-22 20:29:15 +0530

    Recover Binary Search Tree

--- src/binary_search/RecoverBinarySearchTree.java
+++ src/binary_search/RecoverBinarySearchTree.java
@@ -2,4 +2,44 @@ package binary_search;
 
 public class RecoverBinarySearchTree {
 
+    public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+   }
+
+    private TreeNode prev = null , first = null , sec = null;
+    public void recoverTree(TreeNode root) {
+        helper(root);
+        swap(first , sec);
+        return;
+    }
+
+    private void swap (TreeNode a , TreeNode b){
+        int temp = a.val;
+        a.val = b.val;
+        b.val = temp;
+    }
+
+    private void helper (TreeNode root){
+        if(root == null)
+            return;
+
+        helper(root.left);
+
+        if(prev != null && prev.val > root.val){
+            if(first == null)
+                first = prev;
+            sec = root;
+        }
+        prev = root;
+        helper(root.right);
+    }
 }
