d0e008dc48cefa176bb17a5ec356b1db5becff7a
Sayantan <sayantan.das208@gmail.com>
2023-08-24 08:28:20 +0530

    Delete Node in a BST

--- src/binary_search/DeleteNodeinBST.java
+++ src/binary_search/DeleteNodeinBST.java
@@ -1,6 +1,20 @@
 package binary_search;
 
 public class DeleteNodeinBST {
+
+    public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+    }
+
     public TreeNode deleteNode(TreeNode root, int key) {
         if(root==null) return root;
         if(key < root.val)
