e43ebdcbbb039227d88e4a96e635b1363f7069c6
Sayantan <sayantan.das208@gmail.com>
2023-08-25 20:19:06 +0530

    Validate Binary Search Tree

--- src/binary_search/ValidateBinarySearchTree.java
+++ src/binary_search/ValidateBinarySearchTree.java
@@ -1,6 +1,22 @@
 package binary_search;
 
+import java.util.Stack;
+
 public class ValidateBinarySearchTree {
+
+    public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+    }
+
     public boolean isValidBST(TreeNode root) {
         if (root == null) return true;
         Stack<TreeNode> stack = new Stack<>();
