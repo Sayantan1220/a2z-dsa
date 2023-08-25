f4fa5304f4b47ccc7a7e21809ac0aa503f86acdd
Sayantan <sayantan.das208@gmail.com>
2023-08-25 20:20:29 +0530

    Validate Binary Search Tree

--- src/binary_search/ValidateBinarySearchTree.java
+++ src/binary_search/ValidateBinarySearchTree.java
@@ -1,3 +1,15 @@
+/*
+Given the root of a binary tree, determine if it is a valid binary search tree (BST).
+A valid BST is defined as follows:
+The left subtree of a node contains only nodes with keys less than the node's key.
+The right subtree of a node contains only nodes with keys greater than the node's key.
+Both the left and right subtrees must also be binary search trees.
+https://leetcode.com/problems/validate-binary-search-tree/description/
+
+Approach:
+https://leetcode.com/problems/validate-binary-search-tree/solutions/32112/learn-one-iterative-inorder-traversal-apply-it-to-multiple-tree-questions-java-solution/
+ */
+
 package binary_search;
 
 import java.util.Stack;
