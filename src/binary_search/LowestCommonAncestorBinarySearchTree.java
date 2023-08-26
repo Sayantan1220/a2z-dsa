5271f2cf7f2d00f944d7eaa29ee5cac8a68e15af
Sayantan <sayantan.das208@gmail.com>
2023-08-26 17:07:13 +0530

    Lowest Common Ancestor of a Binary Search Tree

--- src/binary_search/LowestCommonAncestorBinarySearchTree.java
+++ src/binary_search/LowestCommonAncestorBinarySearchTree.java
@@ -2,6 +2,13 @@ package binary_search;
 
 public class LowestCommonAncestorBinarySearchTree {
 
+    public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode(int x) { val = x; }
+    }
+
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         while(root != null){
             if(root.val > p.val && root.val > q.val){
