3bc200bb432d8414108ff86064ba756444865192
Sayantan <sayantan.das208@gmail.com>
2023-08-27 12:16:07 +0530

    Binary Search Tree Iterator

--- src/binary_search/BinarySearchTreeIterator.java
+++ src/binary_search/BinarySearchTreeIterator.java
@@ -1,4 +1,25 @@
 package binary_search;
 
 public class BinarySearchTreeIterator {
+    Stack<TreeNode> stack;
+    public BSTIterator(TreeNode root) {
+        stack = new Stack<>();
+        TreeNode node = root;
+        updateStack(node);                                      // update stack
+    }
+    public int next() {
+        TreeNode toRemove = stack.pop();
+        updateStack(toRemove.right);                             // before return node, first update stack further
+        return toRemove.val;
+    }
+    public boolean hasNext() {
+        return !stack.isEmpty();
+    }
+    // -------------------
+    public void updateStack(TreeNode node){
+        while(node != null){
+            stack.add(node);
+            node = node.left;
+        }
+    }
 }
