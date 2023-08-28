7415ed60441fb7d0c918c568af2c7ee325202f7f
Sayantan <sayantan.das208@gmail.com>
2023-08-28 22:34:36 +0530

    Two Sum IV - Input is a BST

--- src/binary_search/TwoSumIVInputBST.java
+++ src/binary_search/TwoSumIVInputBST.java
@@ -2,6 +2,9 @@
 Given the root of a binary search tree and an integer k,
 return true if there exist two elements in the BST such that their sum is equal to k, or false otherwise.
 https://leetcode.com/problems/two-sum-iv-input-is-a-bst/description/
+
+TC : O(N)
+SC : O(N)
  */
 
 package binary_search;
