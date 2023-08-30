d2f481dd8c7b4e1d9dadee5cdc614c5cb68e2b65
Sayantan <sayantan.das208@gmail.com>
2023-08-30 23:44:35 +0530

    Symmetric Tree

--- src/binary_tree/SymmetricTree.java
+++ src/binary_tree/SymmetricTree.java
@@ -1,8 +1,22 @@
 /*
 Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).
  https://leetcode.com/problems/symmetric-tree/description/
+
+Intuition
+To check if a binary tree is symmetric, we need to compare its left subtree and right subtree.
+To do this, we can traverse the tree recursively and compare the left and right subtrees at each level.
+If they are symmetric, we continue the traversal. Otherwise, we can immediately return false.
+
+Approach
+We can define a recursive helper function that takes two nodes as input, one from the left subtree
+and one from the right subtree. The helper function returns true if both nodes are null,
+or if their values are equal and their subtrees are symmetric.
+
+TC : O(n)
+SC : O(h)
  */
- */
+
+
 package binary_tree;
 
 public class SymmetricTree {
