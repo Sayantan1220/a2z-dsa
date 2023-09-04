e3c43b5e5d0626df7df0e1c857b11d9036bcb873
Sayantan <sayantan.das208@gmail.com>
2023-09-04 19:03:50 +0530

    Binary Tree Inorder Traversal

--- src/binary_tree/BinaryTreeInorderTraversal.java
+++ src/binary_tree/BinaryTreeInorderTraversal.java
@@ -1,6 +1,23 @@
 /*
 Given the root of a binary tree, return the inorder traversal of its nodes' values.
 https://leetcode.com/problems/binary-tree-inorder-traversal/description/
+
+Approach
+When we are currently at a node, the following cases can arise:
+Case 1: When the current node has no left subtree. In this scenario, there is nothing to be traversed on the left side, so we simply print the value of the current node and move to the right of the current node.
+Case 2: When there is a left subtree and the right-most child of this left subtree is pointing to null. In this case we need to set the right-most child to point to the current node, instead of NULL and move to the left of the current node.
+Case 3: When there is a left subtree and the right-most child of this left-subtree is already pointing to the current node. In this case we know that the left subtree is already visited so we need to print the value of the current node and move to the right of the current node.
+Note: Case 3 is very important as we need to remove the new links added to restore the original tree.
+
+To summarize, at a node whether we have to move left or right is determined whether the node has a left subtree.
+If it doesn’t we move to the right. If there is a left subtree then we see its rightmost child.
+If the rightmost child is pointing to NULL, we move the current node to its left.
+If the rightmost child is already pointing towards the current node, we remove that link
+and move to the right of the current node. We will stop the execution when the current points to null
+and we have traversed the whole tree.
+
+TC : O(n)
+SC : O(1)
  */
 
 package binary_tree;
