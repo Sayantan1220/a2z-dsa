09c494ecd0dbf2a5ff7d39c648683faf7ec90b60
Sayantan <sayantan.das208@gmail.com>
2023-09-04 19:00:21 +0530

    Binary Tree Inorder Traversal

--- src/binary_tree/BinaryTreeInorderTraversal.java
+++ src/binary_tree/BinaryTreeInorderTraversal.java
@@ -1,6 +1,20 @@
 package binary_tree;
 
 public class BinaryTreeInorderTraversal {
+
+    public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+    }
+    
     public List<Integer> inorderTraversal(TreeNode root) {
         List<Integer>ans=new ArrayList<>();
         TreeNode curr=root;
