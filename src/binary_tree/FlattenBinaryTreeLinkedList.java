13733fe2012db5001a9d90b396de821478449ba2
Sayantan <sayantan.das208@gmail.com>
2023-09-05 21:27:30 +0530

    Flatten Binary Tree to Linked List

--- src/binary_tree/FlattenBinaryTreeLinkedList.java
+++ src/binary_tree/FlattenBinaryTreeLinkedList.java
@@ -1,4 +1,17 @@
 package binary_tree;
 
 public class FlattenBinaryTreeLinkedList {
+    public void flatten(TreeNode root) {
+        TreeNode curr = root;
+        while (curr != null) {
+            if (curr.left != null) {
+                TreeNode runner = curr.left;
+                while (runner.right != null) runner = runner.right;
+                runner.right = curr.right;
+                curr.right = curr.left;
+                curr.left = null;
+            }
+            curr = curr.right;
+        }
+    }
 }
