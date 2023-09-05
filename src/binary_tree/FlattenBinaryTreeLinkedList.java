a7ff3bc4da9e16da6af9e8889a5bbd4fbb6c96fa
Sayantan <sayantan.das208@gmail.com>
2023-09-05 21:28:54 +0530

    Flatten Binary Tree to Linked List

--- src/binary_tree/FlattenBinaryTreeLinkedList.java
+++ src/binary_tree/FlattenBinaryTreeLinkedList.java
@@ -1,6 +1,28 @@
+/*
+Given the root of a binary tree, flatten the tree into a "linked list":
+The "linked list" should use the same TreeNode class where the right child pointer points to the next node
+in the list and the left child pointer is always null.
+The "linked list" should be in the same order as a pre-order traversal of the binary tree.
+https://leetcode.com/problems/flatten-binary-tree-to-linked-list/description/
+ */
+
 package binary_tree;
 
 public class FlattenBinaryTreeLinkedList {
+
+    public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+    }
+    
     public void flatten(TreeNode root) {
         TreeNode curr = root;
         while (curr != null) {
